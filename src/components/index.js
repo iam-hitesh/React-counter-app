02a9ce193403928a1885dc545c6287eadd5a3f49
Hitesh Yadav <hhiteshyadav@gmail.com>
2020-06-08 00:09:58 +0530

    Build version

--- src/components/index.js
+++
@@ -1,2 +0,0 @@
-export { default as NavBar } from './navbar/index';
-export { default as Counters } from './counters/index';
--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-
-import CounterReducer from "./counter";
-
-export default combineReducers({
-    counter: CounterReducer,
-});
